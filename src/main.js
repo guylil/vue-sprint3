0bbe46df9cd5d056c81a2b00af7e8fb120b0b5cf
guylil <guylilian@gmail.com>
2016-12-25 16:16:06 +0200

    adding the server func

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue              from 'vue';
-// import VueResource      from 'vue-resource';
+import VueResource      from 'vue-resource';
 import VueRouter        from 'vue-router';
 import App              from './App.vue';
 
@@ -7,14 +7,14 @@ import style            from './style.scss';
 import MainNav          from './components/main-nav';
 import routes           from './routes.js';
 
-// Vue.use(VueResource);
+Vue.use(VueResource);
 Vue.use(VueRouter);
 
 const router = new VueRouter({
   mode: 'history',
   routes
 })
-// Vue.http.options.root = 'http://localhost:3000';
+Vue.http.options.root = 'http://localhost:3000';
 
 const app = new Vue({
   router,
